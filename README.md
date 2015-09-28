6c3028c1f2b55abaca89cfabc7f3d49714adf456
Steve Veerman <steve@veerman.ca>
2015-09-28 01:10:13 -0400

    Auto 64/32-bit Ghostscript detection. Added basic error handling.

--- README.md
+++ README.md
@@ -5,13 +5,15 @@ A simple Windows batch file which uses [Ghostscript](http://www.ghostscript.com/
 [Steve Veerman](http://steve.veerman.ca/)
 
 ## Instructions
-* Download [Postscript and PDF interpreter/renderer](http://ghostscript.com/download/gsdnld.html) (64 bit version preferred)
+* Download [Postscript and PDF interpreter/renderer](http://ghostscript.com/download/gsdnld.html) (Windows 64-bit or 32-bit)
 * Run pdf_unlock.bat
 * Enter full path to pdf file eg. C:\Users\You\Desktop\locked.pdf
 * Done
 
 ## Notes
-* For x86 (32 bit), you will need to comment/uncomment x86/x64 blocks and change set gs_path to point to x86
+* This script works with either 64-bit or 32-bit Ghostscript (will detect automatically).
 
 ## Inspiration
-* [This Reddit comment](https://www.reddit.com/r/sysadmin/comments/1vyby5/any_way_to_recoverremove_password_from_pdf/cewyp3g)
+* [Reddit comment](https://www.reddit.com/r/sysadmin/comments/1vyby5/any_way_to_recoverremove_password_from_pdf/cewyp3g)
+* [removing-password-from-pdf-on-linux](http://www.cyberciti.biz/faq/removing-password-from-pdf-on-linux/)
+* [KevCJones/howto-unlock-pdf](https://gist.github.com/KevCJones/4483127)
--- unlock_pdf.bat
+++ unlock_pdf.bat
@@ -1,22 +1,51 @@
 @echo off
+rem Windows Batch Script created by Steve Veerman
+
 set /p encrypted_pdf_path="PDF File (use full path eg. C:\Users\You\Desktop\locked.pdf): "
-REM echo %encrypted_pdf_path%
+echo.
+rem echo %encrypted_pdf_path%
+
+if not exist %encrypted_pdf_path% (
+	echo PDF File not found. Please try again.
+	goto done
+)
 
 call set decrypted_pdf_path=%%encrypted_pdf_path:.pdf=.unencrypted.pdf%%
-echo.
-echo Attempting to save %decrypted_pdf_path%
-echo.
+echo Saving %decrypted_pdf_path%
 
-REM set gs_path_x86=
-REM for /f "delims=" %%a in ('where /R "c:\Program Files (x86)" gswin*c.exe') do @set gs_path_x86=%%a
-REM echo x86 %gs_path_x86%
+set gs_exec="gswin*c.exe"
 
 set gs_path_x64=
-for /f "delims=" %%a in ('where /R "c:\Program Files" gswin*c.exe') do @set gs_path_x64=%%a
-REM echo x64 %gs_path_x64%
+for /f "delims=" %%a in ('where /R "c:\Program Files" %gs_exec%') do @set gs_path_x64=%%a
+rem echo x64 path="%gs_path_x64%"
 
-set gs_path=%gs_path_x64%
+if not ["%gs_path_x64%"] == [""] (
+	rem echo is x64
+	set gs_path=%gs_path_x64%
+	goto run_gs
+)
 
+set gs_path_x86=
+for /f "delims=" %%a in ('where /R "c:\Program Files (x86)" %gs_exec%') do @set gs_path_x86=%%a
+rem echo x86 path="%gs_path_x86%"
+
+if not ["%gs_path_x86%"] == [""] (
+	rem echo is x86
+	set gs_path=%gs_path_x86%
+	goto run_gs
+)
+
+echo %gs_exec% not found. Please install Ghostscript.
+goto done
+
+:run_gs
+rem echo Working...
 "%gs_path%" -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile="%decrypted_pdf_path%" -c .setpdfwrite -f "%encrypted_pdf_path%"
+goto done
 
+:done
+echo.
 echo Done.
+echo.
+pause
+exit /B
